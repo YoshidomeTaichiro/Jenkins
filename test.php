b8503d80996e217832187507486c4755279de9bc
YoshidomeTaichiro <taichiro_yoshidome@rocksystem.co.jp>
2020-04-01 18:03:20 +0900

    test

---
+++ test.php
@@ -0,0 +1,15 @@
+<form action="#" 
+    onsubmit="alert('テストのため送信しません');return false;" 
+    onreset="alert('フォームをリセットします')">
+    <p>
+        <input type="text" value="" />
+    </p>
+    <p>
+        <button type="button" id="normalButton">タイプボタン</button>
+
+        <button>ボタン</button>
+        <button type="submit">サブミット</button>
+
+        <button type="reset">リセットボタン</button>
+    </p>
+</form>
